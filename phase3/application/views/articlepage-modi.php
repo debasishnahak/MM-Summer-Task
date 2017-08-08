21a2e9b86344652879e6164d8c669445ed1d6e35
debasishnahak <debasishnahak99@gmail.com>
2017-08-08 22:29:24 +0530

    Add files via upload

---
+++ phase3/application/views/articlepage-modi.php
@@ -0,0 +1,356 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Indigo Express</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+ <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<style>
+.indigo {
+	color: chartreuse;
+	font-size: 32px;
+	display: inline;
+	padding-bottom:0px;
+}
+.express {
+	color: aliceblue;
+	font-size:30px;
+	display: inline;
+	padding-bottom:0px;
+}
+
+h3 {
+	padding-left: 20px;
+	padding-bottom: 5px;
+	padding-top: 5px;
+	margin-left:20px;
+	border-style: solid;
+	border-color:cyan;
+	width:270px;
+	}
+h2 {
+	font-size:20px;
+	
+	}
+h6 {
+	font-size:15px;	
+}	
+h5{
+font-size:16px;
+color:#D3D3D3;}
+p {
+
+	font-size:17px;
+	text-align: justify;
+}
+	
+.headed  {
+	display: inline;
+}
+hr { 
+    display: block;
+    margin-top: 0.5em;
+    margin-bottom: 0.5em;
+    margin-left: auto;
+    margin-right: auto;
+    border-style: inset;
+    border-width: 2px;
+}
+
+
+}
+
+.pictures{
+	
+	padding-top:15px;
+	padding-bottom:30px;
+}
+ul {
+    list-style-type: none;
+    overflow: display;
+    background-color: #333;
+}
+li a {
+    display: block;
+    color: white;
+    text-align: center;
+    text-decoration: none;
+}
+.log {
+float:right;
+padding-right:40px;
+font-size: 17px;
+display:block;
+color: yellow;
+}
+.body{
+	background-color: aquamarine;
+
+}
+.text {
+  color: maroon;
+  font-size: 30px;
+  padding: 200px 20px;
+  position: absolute;
+  bottom: 8px;
+  width: 100%;
+  text-align: center;
+}
+.connect{
+	display:inline;
+	font-size:10px;
+	padding:20px 20px 20px 20px;
+}
+.footer1{
+	padding-left:20px;
+	padding-bottom:10px;
+}
+.footerpar{
+	color:white;
+	font-size:14px;
+}
+.navbar {
+    margin: 0;
+	
+}
+.privacy {
+	float:left;
+	padding-left:110px;
+	
+}
+.vr {
+    width:1px;
+    background-color:ivory;
+    position:absolute;
+    top:10px;
+    bottom:110px;
+    left:440px;
+}
+</style>
+<body>
+	<div class="navbar navbar-inverse">	
+			<h3><div class="indigo"><strong>Indigo</strong></div>-<div class="express"><strong>express</strong></div></h3>
+			<a href='signup.html'><div class="log"><span class='glyphicon glyphicon-user'></span> 
+			<strong>SIGN UP</strong></div></a>
+			 <a href='login.html'><div class="log"><span class='glyphicon glyphicon-log-in'></span> 
+			<strong>LOG IN</strong></div></a>
+			
+	</div>
+		<div class="navbar navbar-inverse">
+			<div class='collapse navbar-collapse' id='myNavbar'>
+			<ul class="nav nav-pills">
+				<li><a class="active" href="homepage.html"><i class="fa fa-home" ></i> Home</a></li>
+				<li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">national
+				<b class="caret"></span></b>
+				</a>
+				<ul class="dropdown-menu">
+					<li><a href="#">delhi</a></li>
+					<li><a href="#">mumbai</a></li>
+					<li><a href="#">hawarah</a></li>
+					<li><a href="#">chennai</a></li>
+				</ul>
+				</li>
+				<li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">international
+				<b class="caret"></span></b>
+				</a>
+				<ul class="dropdown-menu">
+					<li><a href="#">USA</a></li>
+					<li><a href="#">Australia</a></li>
+					<li><a href="#">Spain</a></li>
+					<li><a href="#">Afganistan</a></li>
+				</ul>
+				</li>
+				
+				<li><a href="#about">bollywood</a></li>
+				
+				<li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#about">fashion
+				<b class="caret"></span></b></a>
+				<ul class="dropdown-menu">
+					<li><a href="#">north</a></li>
+					<li><a href="#">south</a></li>
+					<li><a href="#">east</a></li>
+					<li><a href="#">west</a></li>
+					
+				</ul>
+				</li>
+				<li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#about">sports
+				<b class="caret"></span></b>
+				</a>
+				<ul class="dropdown-menu">
+					<li><a href="#">cricket</a></li>
+					<li><a href="#">hockey</a></li>
+					<li><a href="#">football</a></li>
+					<li><a href="#">golf</a></li>
+				</ul>
+				</li>	
+		
+				<li><a href="#about">Science & technology</a></li>
+				
+				<li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">markets
+				<b class="caret"><span></b>
+				</a>
+				<ul class="dropdown-menu">
+					<li><a href="#">bonds</a></li>
+					<li><a href="#">commercial real estate</a></li>
+					<li><a href="#">commodities & features</a></li>
+					<li><a href="#">curencies</a></li>
+				</ul>
+				</li>
+				<li class="searching">
+			<form class="navbar-form navbar-right" role="search">
+				<div class="form-group">
+				<input type="text" class="form-control" placeholder="Search">
+				</div>
+				<button type="submit" class="btn btn-default"><span class="glyphicon glyphicon-search"></span></button>
+			</form>
+			</li>
+			</ul>
+			</div>
+		</div>
+		<br>
+<div class="container">		
+	<div class="row">
+		<div class="col-md-7">
+				<h1>PM Modi Speaks At Madison Square Garden: Highlights</h1>
+				<img src="pictures/modi.jpg" width="650">
+				<p>NEW YORK:  Prime Minister Narendra Modi, in a saffron jacket, got a full-house reception on Sunday at New York's Madison Square Garden, a venue that rock stars dream of packing with a sell-out audience. He thanked the crowd of about 18,000 Indian Americans who constantly chanted his name, for the rousing welcome.
+				<br>Here are the highlights of his speech:
+				<br>
+				<ol>
+					<li>Prime Minister Narendra Modi arrives at Madison Square Garden</li>
+					<li>Kavita Krishnamurthy sings the national anthem</li>
+					<li>Mr Modi begins his address with 'Bharat Mata ki Jai'</li>
+					<li>My greetings to you on the occasion of Navratri. I am fortunate to have this opportunity to meet you on such an auspicious occasions</li>
+					<li>India used to be known as a country of snakes and snake charmers</li>
+					<li>It is only because of you (Indian-American community) that we have made such a huge progress</li>
+					<li>If not for you all,there wouldn't have been an IT revolution</li>
+					<li>You (Indian-American community) have earned a lot of respect in USA through skills and values</li>
+					<li>Through your actions and values you have earned immense respect</li>
+					<li>I visited Taiwan once and an interpreter said your country has been a land of snakes and snake charmers</li>
+					<li>I said 'No, we have been vastly under-valued. Our ancestors used to play with snakes, we play with mouse.'</li>
+					<li>Many of you didn't get a chance to vote, but when the results came, not one of you must have been sleeping</li>
+					<li>Many of you were involved in the election process</li>
+					<li>You may not have voted in 2014 but when results were coming in, you celebrated</li>
+					<li>Today, I am here to thank you all</li>
+					<li>You have a huge contribution in building India's image.</li>
+					<li>Winning an election is not just about post or chair, it's a responsibility</li>
+					<li>I haven't taken a 15-minute break since I took over.</li>
+					<li>For the first time in 30 years, a party won a clear majority in India</li>
+					<li>Even the opinion makers could not make an opinion</li>
+					<li>You all have several expectations from India. Even the people of India are expecting a lot from our government.</li>
+					<li>We will be successful in fulfilling the aspirations of people</li>
+					<li>The world is convinced that 21st century belongs to Asia</li>
+					<li>We are a youthful nation with a very old culture. 65 per cent of Indians are below the age of 35.</li>
+					<li>There is no reason to be disappointed. India will progress very fast and the skills of our youth will take the country ahead.</li>
+					<li>When 125 crore people give their blessings, it automatically becomes God's blessing.</li>
+					<li>We have three powers that nobody else has, and it is our duty to identify these powers, present it to the World and mobilise it:
+					Democracy - This is our biggest strength. Even the poorest man went out in the heat to hear speeches
+					Demographic dividend - 65% of people less than the age of 35 years
+					Demand - The World is looking at India</li>
+					<li>Democracy is India's biggest strength</li>
+					<li>Mahatma Gandhi made the freedom struggle a mass movement. Every Indian felt that yes, I am also a part of the freedom struggle. We want to make development a mass movement.</li>
+					<li>The world needs teachers, we will supply teachers</li>
+					<li>Mahatma Gandhi was an expatriate who returned to India. It will be 100 years of that event next year. Pravasi Bahartiya Divas will be held in Ahmedabad this time</li>
+					<li>Those who stay in India for a long time had to report to police stations. They will not have to do so.</li>
+				</ol>
+				</p>	
+		</div>
+	<div class="row">	
+		<div class="col-md-offset-1 col-md-4">
+			<div class="jumbotron"><h1>weather</h1>
+			<h2>city weather conditions</h2>
+			
+			</div>
+		</div>
+		<div class="col-md-offset-1 col-md-4">
+			<div class="jumbotron">
+			<h1>videos</h1>
+			<h2>videos related to important issue</h2>
+			</div>
+		</div>
+		<div class="col-md-offset-1 col-md-4">
+			<div class="jumbotron"><h1>polling analysis</h1></div>
+		</div>
+	</div>
+	</div>	
+	</div>
+	
+		<div class="navbar navbar-inverse">
+		<div class="row">
+		<div class="col-md-4">
+		<div class="footer1">
+			<h3><div class="indigo"><strong>Indigo</strong></div>-<div class="express"><strong>express</strong></div></h3>
+			<p class="footerpar">Indiogo Express is your news,entertainment,music fashion website. We provide you 
+			with the latest breaking news and videos straight from the entertainment industry.
+			</p>
+			<h4><strong style="color:aqua;">Contact us:</strong><a href="#">mail@indigoexpress.com</a></h4><br>
+				<div class="connect"><a href='#'><i class="fa fa-twitter fa-2x" aria-hidden="true"></i></a></div>
+				<div class="connect"><a href='#'><i class="fa fa-google-plus fa-2x" aria-hidden="true"></i></a></div>
+				<div class="connect"><a href='#'><i class="fa fa-facebook fa-2x" aria-hidden="true"></i></a></div>
+				<div class="connect"><a href="#"><i class="fa fa-linkedin fa-2x" aria-hidden="true"></i></a></div>
+				<div class="connect"><a href="#"><i class="fa fa-pinterest-p fa-2x" aria-hidden="true"></i></a></div>
+		</div>
+		</div>
+		<div class="vr">&nbsp;</div>
+			<div class="col-md-2">
+			<h6><strong style="color:aqua;">coustomer service</strong></h6>
+			<a href="#"><h6><style="color:aqua;">customer center</style></h6></a>
+			<a href="#"><h6><style="color:aqua;">live help</style></h6></a>
+			<a href="#"><h6><style="color:aqua;">Redesign guided Tour</style></h6></a>
+			<a href="#"><h6><style="color:aqua;">Notice to subscribers</style></h6></a>
+			</div>
+			<div class="col-md-2">
+				<h6><strong style="color:aqua;">Tools & Features</strong></h6>
+					<a href="#"><h6><style="color:aqua;">Emails & Alerts</style></h6></a>
+					<a href="#"><h6><style="color:aqua;">Guides</style></h6></a>
+					<a href="#"><h6><style="color:aqua;">My News</style></h6></a>
+					<a href="#"><h6><style="color:aqua;">RSS Feeds</style></h6></a>
+					<a href="#"><h6><style="color:aqua;">Watchlist</style></h6></a>
+					<a href="#"><h6><style="color:aqua;">Video Center</style></h6></a>
+					<a href="#"><h6><style="color:aqua;">Podcasts</style></h6></a>
+			</div>
+			<div class="col-md-2">
+					<h6><strong style="color:aqua;">Ads</strong></h6>
+					<a href="#"><h6><style="color:aqua;">Advertise</style></h6></a>
+					<a href="#"><h6><style="color:aqua;">Advertise Locally</style></h6></a>
+					<a href="#"><h6><style="color:aqua;">Comercial Real Estate Ads</style></h6></a>
+					<a href="#"><h6><style="color:aqua;">Place a classified Ads</style></h6></a>
+					<a href="#"><h6><style="color:aqua;">Sell your Bussiness</style></h6></a>
+					<a href="#"><h6><style="color:aqua;">Sell your Home</style></h6></a>
+					<a href="#"><h6><style="color:aqua;">Recuirements and career Ads</style></h6></a>
+			</div>
+			
+			<div class="col-md-2">
+					<h6><strong style="color:aqua;">More</strong></h6>
+					<a href="#"><h6><style="color:aqua;">content Partnerships</style></h6></a>
+					<a href="#"><h6><style="color:aqua;">corrections</style></h6></a>
+					<a href="#"><h6><style="color:aqua;">News Archive</style></h6></a>
+					<a href="#"><h6><style="color:aqua;">Resister For free</style></h6></a>
+					<a href="#"><h6><style="color:aqua;">Reprints</style></h6></a>
+			</div>
+			
+		</div>
+		<div class="row">
+		<div class='navbar navbar-inverse'>
+			<center>
+			<div class="privacy"><a href="#"><h5>Privacy Policy</h5></a></div>
+			<div class="privacy"><a href="#"><h5>Cookie Policy</h5></a></div>
+			<div class="privacy"><a href="#"><h5>copyright Policy</h5></a></div>
+			<div class="privacy"><a href="#"><h5>Data Policy</h5></a></div>
+			<div class="privacy"><a href="#"><h5>Subscriber agreement & terms of use</h5></a></div>
+			</center>
+		</div>
+		</div>
+	<div class='row'>
+<div class='navbar navbar-inverse navbar-static-top'>
+<div class='col-md-12'>
+<center><h5>&copy 1998 - 2017 Indigo Express,Inc. All Rights Reserved.</h5></center>
+</div>
+</div>
+</div>
+</body>
+</html>
